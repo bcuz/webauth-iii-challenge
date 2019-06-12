9b2480a6af25912c0994145cce7e3a87bb3831c2
adam <bcuz@users.noreply.github.com>
2019-06-11 21:31:13 -0400

    login with jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
+const secrets = require('./config/secrets.js');
 const Users = require('./data/users-model');
 
 const server = express();
@@ -28,6 +29,49 @@ server.post('/api/register', async (req, res) => {
     });
   }
 });
+
+server.post('/api/login', async (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(401).json({ message: 'Need username and password' });
+  }
+
+  try {
+    let user = await Users.findBy({ username }).first()    
+
+    if (user) {
+      if (bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          authToken: token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+
+    } else {
+      res.status(404).json({ message: 'user not found' });
+    }
+
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the user',
+    });
+  }
+});
+
+function generateToken(user) {
+  return jwt.sign({
+    userId: user.id,
+  }, secrets.jwtSecret, {
+    expiresIn: '1d',
+  })
+}
 
 const port = process.env.PORT || 5001;
 server.listen(port, function() {
